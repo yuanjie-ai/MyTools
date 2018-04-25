8dd6ab8873d7bcd14ae3df2bd498cec78a288c05
尐 <313303303@qq.com>
2018-04-25 19:31:54 +0800

    Create MultiApply.py

---
+++ 0_utils/utils/MultiApply.py
@@ -0,0 +1,15 @@
+class MultiApply(object):
+    """
+    import dask.dataframe as dd
+    from dask.multiprocessing import get
+    """
+
+    def __init__(self, pandas_df):
+        self.pandas_df = pandas_df
+
+    def multiApply(self, func=lambda df: df['xx'].progress_apply(lambda: 1)):
+        return self.ddf.map_partitions(func).compute(get=get)
+
+    @property
+    def ddf(self):
+        return dd.from_pandas(self.pandas_df, 32)
