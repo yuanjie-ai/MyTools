dc086270e648f12c38432dd76adf9257848448cf
尐 <313303303@qq.com>
2018-04-25 19:38:27 +0800

    Update MultiApply.py

--- 0_utils/utils/MultiApply.py
+++ 0_utils/utils/MultiApply.py
@@ -8,11 +8,8 @@ tqdm.pandas(desc='prep')
 class MultiApply(object):
 
     def __init__(self, pandas_df):
-        self.pandas_df = pandas_df
-
-    def multiApply(self, func=lambda df: df['xx'].progress_apply(lambda: 1)):
+        self.ddf = dd.from_pandas(pandas_df, 32)
+        
+    def run(self, func=lambda df: df['xx'].progress_apply(lambda: 1)):
         return self.ddf.map_partitions(func).compute(get=get)
 
-    @property
-    def ddf(self):
-        return dd.from_pandas(self.pandas_df, 32)
