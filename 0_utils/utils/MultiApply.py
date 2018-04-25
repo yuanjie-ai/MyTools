b8aa73093da3fa1c6b6b10d6b776c8c668baa4ce
尐 <313303303@qq.com>
2018-04-25 19:52:11 +0800

    Update MultiApply.py

--- 0_utils/utils/MultiApply.py
+++ 0_utils/utils/MultiApply.py
@@ -9,7 +9,7 @@ class MultiApply(object):
     def __init__(self, pandas_df):
         self.ddf = dd.from_pandas(pandas_df, 32)
 
-    def run(self, func):
+    def apply(self, func):
         """
         :param func: lambda df: df['xx'].progress_apply(func_)
         :return: pd.DataFrame
