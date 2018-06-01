a7bd62655143231adad900d2a0d172a2aa8f9797
尐 <313303303@qq.com>
2018-06-01 17:29:25 +0800

    Update setup.py

--- 99_Pypi/setup.py
+++ 99_Pypi/setup.py
@@ -1,26 +1,25 @@
-# -*- coding:utf-8 -*-
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 with open("README.md") as f:
     long_description = f.read()
-    
+
 setup(
-    name = 'pyTool',
-    version = '1.0.0',
-    url = 'https://github.com/Jie-Yuan',
-    keywords = ["DeepLearning", "313303303@qq.com"],
-    description = ('description'),
-    long_description = long_description,
+    name='pyTool',
+    version='1.0.0',
+    url='https://github.com/Jie-Yuan',
+    keywords=["DeepLearning", "313303303@qq.com"],
+    description=('description'),
+    long_description=long_description,
     long_description_content_type="text/markdown",
-    author = 'JieYuan',
-    author_email = '313303303@qq.com',
-    maintainer = 'JieYuan',
-    maintainer_email = '313303303@qq.com',
-    license = 'MIT',
-    packages = find_packages(),
-    platforms = ["all"],
-    python_requires = '>=2.7, <4',
-    classifiers = [
+    author='JieYuan',
+    author_email='313303303@qq.com',
+    maintainer='JieYuan',
+    maintainer_email='313303303@qq.com',
+    license='MIT',
+    packages=find_packages(),
+    platforms=["all"],
+    python_requires='>=2.7, <4',
+    classifiers=[
         'Development Status :: 4 - Beta',
         'Operating System :: OS Independent',
         'Intended Audience :: Developers',
@@ -34,9 +33,9 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Topic :: Software Development :: Libraries'
     ],
-    
+
     # pip install -r requirements.txt
-    install_requires = [
+    install_requires=[
         'pandas>=0.19.2',
         'numpy>=1.11.3',
         'scipy>=0.18.1',
