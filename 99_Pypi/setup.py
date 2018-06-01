659ed17680be64dc0be9e7ff423e80e3a4b0af44
尐 <313303303@qq.com>
2018-06-01 16:59:43 +0800

    Create setup.py

---
+++ 99_Pypi/setup.py
@@ -0,0 +1,46 @@
+# -*- coding:utf-8 -*-
+from setuptools import setup, find_packages
+
+with open("README.md") as f:
+    long_description = f.read()
+    
+setup(
+    name = 'pyTool',
+    version = '1.0.0',
+    keywords = ["DeepLearning", "313303303@qq.com"],
+    description = (
+        '项目的简单描述'
+    ),
+    long_description = long_description,
+    long_description_content_type="text/markdown",
+    author = 'JieYuan',
+    author_email = '313303303@qq.com',
+    maintainer = 'JieYuan',
+    maintainer_email = '313303303@qq.com',
+    license = 'MIT',
+    packages = find_packages(),
+    platforms = ["all"],
+    url = 'https://github.com/Jie-Yuan',
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries'
+    ],
+    
+    # pip install -r requirements.txt
+    install_requires = [
+        'pandas>=0.19.2',
+        'numpy>=1.11.3',
+        'scipy>=0.18.1',
+        'matplotlib>=2.0.0',
+    ]
+)
