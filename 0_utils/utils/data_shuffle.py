5232a029e699ecb9f051e444f28c68a8425c95b2
尐 <313303303@qq.com>
2018-06-14 17:56:06 +0800

    Update data_shuffle.py

--- 0_utils/utils/data_shuffle.py
+++ 0_utils/utils/data_shuffle.py
@@ -1,5 +1,5 @@
-def data_shuffle(X, y, seed=None):
-    idx = np.arange(len(X))
+def data_shuffle(*Xs, y=None, seed=None):
+    idx = np.arange(len(Xs[0]))
     np.random.seed(seed)
     np.random.shuffle(idx)
-    return X[idx], y[idx]
+    return [X[idx] for X in Xs], y[idx]
