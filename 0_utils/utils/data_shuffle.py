1dad792aa53a94ad06f88b5c8560f11a1219cccf
尐 <313303303@qq.com>
2019-01-22 11:45:11 +0800

    Delete data_shuffle.py

--- 0_utils/utils/data_shuffle.py
+++
@@ -1,7 +0,0 @@
-# from sklearn.model_selection import train_test_split
-
-def data_shuffle(*Xs, y=None, seed=None):
-    idx = np.arange(len(Xs[0]))
-    np.random.seed(seed)
-    np.random.shuffle(idx)
-    return [X[idx] for X in Xs], y[idx]
